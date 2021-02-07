4cb71e935c9ceda94495e0938b7acddc20f64165
Kendell R <KTibow@users.noreply.github.com>
2021-02-07 07:22:54 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,24 +1,12 @@
 # Imports
 import time
-from adafruit_magtag.magtag import MagTag
-# Wifi related
-import adafruit_requests
-import wifi
-import ssl
-import socketpool
 from secrets import secrets
-# Setup
-magtag = MagTag()
-magtag.peripherals.neopixel_disable = False
-magtag.peripherals.neopixels.fill((100, 0, 100))
-# WiFi setup
-wifi.radio.connect(secrets["ssid"], secrets["password"])
-pool = socketpool.SocketPool(wifi.radio)
-requests = adafruit_requests.Session(pool, ssl.create_default_context())
-magtag.peripherals.neopixels.fill((0, 0, 0))
-magtag.peripherals.neopixel_disable = True
-# Loop
-while True:
-  response = requests.get(secrets["endpoint"] + "/api/stock")
-  raise Exception(response.text)
-  time.sleep(0.01)
+def main():
+    # Setup
+    magtag.peripherals.neopixel_disable = False
+    magtag.peripherals.neopixels.fill((100, 0, 100))
+    # Loop
+    while True:
+      response = requests.get(secrets["endpoint"] + "/api/stock")
+      raise Exception(response.text)
+      time.sleep(0.01)
