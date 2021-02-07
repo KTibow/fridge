ac86916cdbbb73364bb2f79fbc81966499f2884f
Kendell R <KTibow@users.noreply.github.com>
2021-02-07 10:02:01 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 # Imports
 import time
+import terminalio
 from secrets import secrets
 
 
@@ -7,9 +8,22 @@ def main():
     # Setup
     magtag.peripherals.neopixel_disable = False
     magtag.peripherals.neopixels.fill((100, 0, 100))
+    time = ""
+    weather = ""
+    overdue = ""
+    leftovers = ""
+    magtag.add_text(
+        text_font=terminalio.FONT,
+        text_position=(
+            50,
+            (magtag.graphics.display.height // 2) - 1,
+        ),
+        text_scale=1,
+    )
+    magtag.set_text("It's 4:03 PM.")
     # Loop
     while True:
-        if not magtag.peripherals.buttons[1].value:
+        if magtag.peripherals.any_button_pressed:
             magtag.peripherals.neopixels.fill((0, 100, 100))
             time.sleep(1)
             break
