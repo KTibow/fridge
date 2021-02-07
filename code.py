aff09a19728535fdbe482d826740a1ea7221bfe5
Kendell R <KTibow@users.noreply.github.com>
2021-02-07 08:57:04 -0800

    Update code.py

--- code.py
+++ code.py
@@ -9,8 +9,9 @@ def main():
     magtag.peripherals.neopixels.fill((100, 0, 100))
     # Loop
     while True:
-        if magtag.peripherals.buttons[0]:
+        if not magtag.peripherals.buttons[0]:
             magtag.peripherals.neopixels.fill((0, 100, 100))
+            break
         response = requests.get(
             secrets["endpoint"] + "/api/stock",
             headers={
