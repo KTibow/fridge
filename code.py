ecb728e43d44361e4aadfc1ecf089a1013f42c6c
Kendell R <KTibow@users.noreply.github.com>
2021-02-07 07:40:13 -0800

    Update code.py

--- code.py
+++ code.py
@@ -8,9 +8,14 @@ def main():
     magtag.peripherals.neopixel_disable = False
     magtag.peripherals.neopixels.fill((100, 0, 100))
     # Loop
-    response = requests.get(
-        secrets["endpoint"] + "/api/stock",
-        headers={"GROCRY-API-KEY": secrets["api_key"]},
-    )
-    raise Exception(response.text)
-    time.sleep(0.01)
+    while True:
+        response = requests.request(
+            "GET",
+            secrets["endpoint"] + "/api/stock",
+            headers={
+                "GROCRY-API-KEY": secrets["api_key"],
+                "accept": "application/json",
+            },
+        )
+        raise Exception(response.text)
+        time.sleep(0.01)
