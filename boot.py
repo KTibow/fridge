f7cfcf60de747b954c390633bb12aeb4008cc851
Kendell R <KTibow@users.noreply.github.com>
2021-02-07 09:53:15 -0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,20 +1,8 @@
-# Initial message
-from adafruit_magtag.magtag import MagTag
-import terminalio
-
-magtag = MagTag()
-magtag.add_text(
-    text_font=terminalio.FONT,
-    text_position=(
-        50,
-        (magtag.graphics.display.height // 2) - 1,
-    ),
-    text_scale=3,
-)
-magtag.peripherals.neopixels.fill((255, 0, 0))
-# Everything else
+# Imports
 import time
 import storage
+from adafruit_magtag.magtag import MagTag
+import code
 
 # Wifi related
 import adafruit_requests
@@ -23,32 +11,38 @@ import ssl
 import socketpool
 from secrets import secrets
 
+magtag = MagTag()
 magtag.peripherals.neopixel_disable = False
-storage.remount("/", False)
+magtag.peripherals.neopixels.fill((255, 75, 0))
+# Init wifi
+wifi.radio.connect(secrets["ssid"], secrets["password"])
+pool = socketpool.SocketPool(wifi.radio)
+requests = adafruit_requests.Session(pool, ssl.create_default_context())
+magtag.peripherals.neopixels.fill((50, 0, 255))
+# Put stuff into the main code
+code.magtag = magtag
+code.requests = requests
+# Download
+response = requests.get(
+    "https://raw.githubusercontent.com/KTibow/fridge/main/code.py"
+)
 
-try:
-    magtag.peripherals.neopixels.fill((255, 75, 0))
-    # Init
-    wifi.radio.connect(secrets["ssid"], secrets["password"])
-    pool = socketpool.SocketPool(wifi.radio)
-    requests = adafruit_requests.Session(pool, ssl.create_default_context())
-    magtag.peripherals.neopixels.fill((50, 0, 255))
-    # Download
-    response = requests.get(
-        "https://raw.githubusercontent.com/KTibow/fridge/main/code.py"
-    )
-    if magtag.peripherals.buttons[0].value:
+if magtag.peripherals.buttons[0].value:
+    try:
+        storage.remount("/", False)
         with open("/code.py", "w") as test:
             # Write
             magtag.peripherals.neopixels.fill((0, 255, 0))
             test.write(response.text)
             test.flush()
-finally:
+    except Exception as e:
+        print("Exception thrown while updating.")
+        print(e)
     storage.remount("/", True)
-    magtag.peripherals.neopixels.fill((0, 0, 0))
-    magtag.peripherals.neopixel_disable = True
-    import code
 
-    code.magtag = magtag
-    code.requests = requests
-    code.main()
+
+magtag.peripherals.neopixels.fill((0, 0, 0))
+magtag.peripherals.neopixel_disable = True
+
+# Finally code
+code.main()
