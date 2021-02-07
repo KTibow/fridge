3ba5087e7651059aa4d6c8dde4c934859d0297f5
Kendell R <KTibow@users.noreply.github.com>
2021-02-07 10:58:56 -0800

    Update code.py

--- code.py
+++ code.py
@@ -15,6 +15,12 @@ def update():
         "http://worldtimeapi.org/api/ip"
     ).json()
     current_time = response["unixtime"] + response["raw_offset"]
+    current_time = time.localtime(current_time)
+    current_time = (
+        current_time.tm_hour % 12 +
+        ":" +
+        current_time.tm_minute 
+    )
     response = requests.get(
         secrets["endpoint"] + "/api/stock",
         headers={
@@ -44,7 +50,7 @@ def main():
             magtag.peripherals.neopixels.fill((0, 100, 100))
             time.sleep(1)
             break
-        if time.monotonic() - last_update > 15:
+        if time.monotonic() - last_update > 20:
             update()
             magtag.set_text(current_time)
             last_update = time.monotonic()
