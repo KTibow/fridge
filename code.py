3401fadb3eefa33c3ec1226ccf8c461c284ef5be
Kendell R <KTibow@users.noreply.github.com>
2021-02-08 12:52:02 -0800

    Update code.py

--- code.py
+++ code.py
@@ -9,6 +9,13 @@ import socketpool # Set up a pool of sockets
 import ssl # Securely connect to APIs
 import adafruit_requests # Actually talk to APIs
 
+# Global stuff
+current_time = ""
+
+# Functions
+def update_time():
+    global current_time
+
 # Initialize
 magtag = MagTag()
 magtag.add_text(
@@ -32,25 +39,24 @@ magtag.add_text(
     ),
     text_scale=1,
 )
-magtag.set_text("1: WiFi, 2: Sockets", 1)
+magtag.set_text("1: WiFi, 2: Sockets, 3: Time, 4: Grocy", 1)
 magtag.peripherals.neopixels[3] = (0, 255, 100)
-magtag.peripherals.neopixels[2] = (0, 255, 100)
 try:
     wifi.radio.connect(secrets["ssid"], secrets["password"])
 except Exception as e:
     magtag.set_text("WiFi error.")
     raise e
 magtag.peripherals.neopixels[3] = (0, 255, 0)
-magtag.peripherals.neopixels[2] = (0, 255, 0)
 # Sockets
-magtag.peripherals.neopixels[1] = (0, 255, 100)
-magtag.peripherals.neopixels[0] = (0, 255, 100)
+magtag.peripherals.neopixels[2] = (0, 255, 100)
 try:
     pool = socketpool.SocketPool(wifi.radio)
     requests = adafruit_requests.Session(pool, ssl.create_default_context())
 except Exception as e:
     magtag.set_text("Socket error.")
     raise e
-magtag.peripherals.neopixels[1] = (0, 255, 0)
-magtag.peripherals.neopixels[0] = (0, 255, 0)
-time.sleep(15)
+magtag.peripherals.neopixels[2] = (0, 255, 0)
+
+# Event loop
+while True:
+    time.sleep(0.01)
