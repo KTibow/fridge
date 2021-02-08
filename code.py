e736f06bb2f1e80fe720ffec0d0fb704b7e1a5e2
Kendell R <KTibow@users.noreply.github.com>
2021-02-08 11:03:42 -0800

    Initial restructure in code.py

--- code.py
+++ code.py
@@ -1,81 +1,56 @@
 # Imports
-import time
-import terminalio
-from secrets import secrets
+from adafruit_magtag.magtag import MagTag  # Control the MagTag
+import terminalio # Get font
+import time # Wait for stuff
+# API-related imports
+from secrets import secrets # WiFi passwords
+import wifi # Connect to WiFi
+import socketpool # Set up a pool of sockets
+import ssl # Securely connect to APIs
+import adafruit_requests # Actually talk to APIs
 
-old_time = ""
-now_time = ""
-overdue = ""
-leftovers = ""
+# Initialize
+magtag = MagTag()
+magtag.add_text(
+    text_font=terminalio.FONT,
+    text_position=(
+        10,
+        10,
+    ),
+    text_scale=3,
+)
+magtag.peripherals.neopixel_disable = False
+magtag.peripherals.neopixels.fill((0, 0, 0))
 
-
-def update_time():
-    # Time
-    global now_time
-    response = requests.get("http://worldtimeapi.org/api/ip").json()
-    now_time = response["datetime"].split("T")[1].split(":")[0:2]
-
-
-def update_food():
-    # Food
-    response = requests.get(
-        secrets["endpoint"] + "/api/stock",
-        headers={
-            "GROCY-API-KEY": secrets["api_key"],
-            "accept": "application/json",
-        },
-    ).json()
-    for food in response:
-        print(food["product"]["name"], food["best_before_date"])
-
-
-def draw():
-    # Time
-    global old_time
-    current_time = [int(time_item) for time_item in now_time]
-    if not current_time:
-        current_time = [0, 0]
-    hours = current_time[0]
-    if hours == 0:
-        hours = 12
-    elif hours > 12 and hours < 24:
-        hours = hours - 12
-    current_time = f"{hours}:{current_time[1]:02}"
-    if old_time != current_time:
-        magtag.set_text(current_time)
-    old_time = current_time
-
-
-def main():
-    # Setup
-    global unix_time
-    magtag.peripherals.neopixels.fill((0, 0, 0))
-    magtag.peripherals.neopixel_disable = True
-    # Previously: (magtag.graphics.display.height // 2) - 1
-    magtag.add_text(
-        text_font=terminalio.FONT,
-        text_position=(
-            10,
-            10,
-        ),
-        text_scale=3,
-    )
-    # Loop
-    last_update = time.monotonic()
-    while True:
-        # Exit
-        if magtag.peripherals.any_button_pressed:
-            magtag.peripherals.neopixels.fill((0, 100, 100))
-            time.sleep(1)
-            break
-        # Update data
-        if time.monotonic() - last_update > 15:
-            try:
-                update_time()
-                update_food()
-            except Exception as e:
-                print("Updating exception:", e)
-            last_update = time.monotonic()
-        # Draw
-        draw()
-        time.sleep(0.2)
+# Connect to WiFi
+magtag.set_text("Connecting...")
+magtag.add_text(
+    text_font=terminalio.FONT,
+    text_position=(
+        10,
+        50,
+    ),
+    text_scale=1,
+)
+magtag.set_text("1: WiFi, 2: Sockets", 1)
+magtag.peripherals.neopixels[3] = (0, 255, 100)
+magtag.peripherals.neopixels[2] = (0, 255, 100)
+try:
+    wifi.radio.connect(secrets["ssid"], secrets["password"])
+except Exception as e:
+    magtag.set_text("WiFi error.")
+    raise e
+magtag.peripherals.neopixels[3] = (0, 255, 0)
+magtag.peripherals.neopixels[2] = (0, 255, 0)
+# Sockets
+magtag.peripherals.neopixels[1] = (0, 255, 100)
+magtag.peripherals.neopixels[0] = (0, 255, 100)
+try:
+    pool = socketpool.SocketPool(wifi.radio)
+    requests = adafruit_requests.Session(pool, ssl.create_default_context())
+except Exception as e:
+    magtag.set_text("Socket error.")
+    raise e
+magtag.peripherals.neopixels[1] = (0, 255, 0)
+magtag.peripherals.neopixels[0] = (0, 255, 0)
+time.sleep(15)
