bed0afac7ed6283fe8dfb7a0df8b3d155c5139b4
Kendell R <KTibow@users.noreply.github.com>
2021-02-08 16:29:41 -0800

    Random stuff

--- code.py
+++ code.py
@@ -13,7 +13,9 @@ import adafruit_requests # Actually talk to APIs
 def update_time():
     global current_time
     response = requests.get("http://worldtimeapi.org/api/ip").json()
-    current_time = response["datetime"].split("T")[1].split(":")[0:2]
+    current_time = response["datetime"].split("T")[1].split(":")
+    current_time = current_time[0:2] + current_time[2].split("-")[0]
+    current_time = [int(time_item) for time_item in current_time]
 
 def update_grocy():
     global current_time
@@ -35,7 +37,6 @@ magtag.peripherals.neopixel_disable = False
 magtag.peripherals.neopixels.fill((0, 0, 0))
 
 # Connect to WiFi
-magtag.set_text("Connecting...")
 magtag.add_text(
     text_font=terminalio.FONT,
     text_position=(
@@ -44,6 +45,7 @@ magtag.add_text(
     ),
     text_scale=1,
 )
+magtag.set_text("Connecting...")
 magtag.set_text("1: WiFi, 2: Sockets, 3: Time, 4: Grocy", 1)
 magtag.peripherals.neopixels[3] = (0, 255, 100)
 try:
@@ -63,8 +65,29 @@ except Exception as e:
 magtag.peripherals.neopixels[2] = (0, 255, 0)
 
 # Global stuff
-last_updated_time = ""
-current_time = ""
+current_time = [3, 14, 15.9]
+time_update_interval = 500
+time_when_time_updated = time_update_interval * -1 # Trigger time update on first run
+grocy_update_interval = 500
+time_when_grocy_updated = grocy_update_interval * -1 # Trigger grocy update on first run
+
+# Initial time pull
+magtag.peripherals.neopixels[1] = (0, 255, 100)
+try:
+    update_time()
+except Exception as e:
+    magtag.set_text("Time API error.")
+    raise e
+magtag.peripherals.neopixels[1] = (0, 255, 0)
+
+# Initial grocy pull
+magtag.peripherals.neopixels[0] = (0, 255, 100)
+try:
+    update_grocy()
+except Exception as e:
+    magtag.set_text("Grocy API error.")
+    raise e
+magtag.peripherals.neopixels[0] = (0, 255, 0)
 
 # Event loop
 while True:
