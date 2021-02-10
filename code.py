55130186d6a5dd83f235ea93aac96a836c105d24
Kendell R <KTibow@users.noreply.github.com>
2021-02-10 12:32:32 -0800

    Update code.py

--- code.py
+++ code.py
@@ -49,10 +49,12 @@ def update_grocy():
             food_is_overdue = False
             if food_date[0] < the_date[0]:
                 food_is_overdue = True
-            elif food_date[1] < the_date[1]:
-                food_is_overdue = True
-            elif food_date[2] <= the_date[2]:
-                food_is_overdue = True
+            elif food_date[0] == the_date[0]:
+                if food_date[1] < the_date[1]:
+                    food_is_overdue = True
+                elif food_date[1] == the_date[1]:
+                    if food_date[2] <= the_date[2]:
+                    food_is_overdue = True
             if food_is_overdue:
                 overdue_food_temp.append(food["product"]["name"])
         overdue_food = overdue_food_temp.copy()
