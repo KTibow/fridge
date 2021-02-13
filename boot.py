e9a9da8e47dd33fe63315dbbe936f57ecee13dfb
Kendell R <KTibow@users.noreply.github.com>
2021-02-13 09:54:19 -0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -8,18 +8,13 @@ import board  # Refresh display
 
 countdown_enabled = False
 try:
-    from animations import countdown  # Countdown
+    from animations import countdown, sin_animate  # Various animations
 
     countdown_enabled = True
 except Exception:
     pass
 
 # OTA-related imports
-from secrets import secrets  # WiFi passwords
-import wifi  # Connect to WiFi
-import socketpool  # Set up a pool of sockets
-import ssl  # Securely connect to GitHub for code
-import adafruit_requests  # Download the code
 import storage  # Mount the code
 import microcontroller  # Reset once it's downloaded
 
@@ -56,10 +51,13 @@ if should_update:
     time.sleep(board.DISPLAY.time_to_refresh)
     magtag.graphics.set_background("installing.bmp")
     board.DISPLAY.refresh()
-    magtag.peripherals.neopixels[3] = (255, 0, 0)
+    magtag.peripherals.neopixels[3] = (125, 25, 255)  # Added brightness - in progress
+    magtag.peripherals.neopixels[2] = (130, 60, 255)  # Added brightness - in progress
+    # I added some extra red (105-130)
+    # And removed some green (75-60) because it looked too white
     # Set up WiFi
     try:
-        wifi.radio.connect(secrets["ssid"], secrets["password"])
+        magtag.network.connect()
     except Exception as e:
         time.sleep(board.DISPLAY.time_to_refresh)
         magtag.graphics.set_background("wifi_error.bmp")
@@ -67,56 +65,55 @@ if should_update:
         while board.DISPLAY.busy:
             pass
         raise e
-    magtag.peripherals.neopixels[2] = (255, 100, 0)
-    # Set up sockets
-    try:
-        pool = socketpool.SocketPool(wifi.radio)
-        requests = adafruit_requests.Session(pool, ssl.create_default_context())
-    except Exception as e:
-        magtag.set_text("Socket error " + e)
-        raise e
+    magtag.peripherals.neopixels[3] = (100, 0, 255)  # Normal brightness - done
+    magtag.peripherals.neopixels[2] = (80, 50, 255)  # Normal brightness - done
     # Download code
-    magtag.peripherals.neopixels[1] = (255, 255, 0)
-    try:
-        main_code_text = requests.get(
-            "https://raw.githubusercontent.com/KTibow/fridge/main/code.py"
-        ).text
-    except Exception as e:
-        print(e)
-        magtag.set_text("Download error " + str(e))
-        raise e
-    try:
-        animations_code_text = requests.get(
-            "https://raw.githubusercontent.com/KTibow/fridge/main/animations.py"
-        ).text
-    except Exception as e:
-        print(e)
-        magtag.set_text("Download error " + str(e))
-        raise e
+    magtag.peripherals.neopixels[1] = (60, 130, 255)  # Added brightness - in progress
+    # I added some extra blue (105-130)
+    # And removed some green (75-60) because it looked too white
+    all_files = []
+    for file_name in ["animations.py", "code.py"]:
+        try:
+            all_files.append(
+                [
+                    file_name,
+                    magtag.network.fetch(
+                        f"https://raw.githubusercontent.com/KTibow/fridge/main/{file_name}"
+                    ).text,
+                ]
+            )
+        except Exception as e:
+            print(e)
+            magtag.set_text("Download error " + str(e))
+            raise e
+    magtag.peripherals.neopixels[1] = (50, 80, 255)  # Normal brightness - done
     # Change code
-    magtag.peripherals.neopixels[0] = (0, 255, 0)
+    magtag.peripherals.neopixels[0] = (25, 125, 255)  # Added brightness - in progress
     try:
         storage.remount("/", False)
-        with open("/code.py", "w") as the_code:
-            the_code.write(main_code_text)
-            the_code.flush()
-        with open("/animations.py", "w") as the_code:
-            the_code.write(animations_code_text)
-            the_code.flush()
+        for file_name, file_content in all_files:
+            with open(f"/{file_name}", "w") as code_file:
+                code_file.write(file_content)
+                code_file.flush()
     except Exception as e:
         print(e)
         magtag.set_text("Changing code error " + str(e))
         raise e
     finally:
         storage.remount("/", True)
+    magtag.peripherals.neopixels[0] = (0, 100, 255)  # Normal brightness - done
     # Tell user that it's done
     time.sleep(board.DISPLAY.time_to_refresh)
     magtag.graphics.set_background("updates_complete.bmp")
     board.DISPLAY.refresh()
     while board.DISPLAY.busy:
-        pass
+        if countdown_enabled:
+            sin_animate(100, 0, 255, 0, 4, magtag)
+    start_time = time.monotonic()
+    while time.monotonic() - start_time < 2:
+        if countdown_enabled:
+            sin_animate(100, 0, 255, 0, 4, magtag)
     magtag.peripherals.neopixels.fill((0, 0, 0))
-    time.sleep(2)
     microcontroller.reset()
 
 magtag.peripherals.neopixel_disable = True
