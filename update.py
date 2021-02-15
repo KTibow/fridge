5dfe5bf778b4dc1a596e1a0780ad4ae18c20e2c9
Kendell R <KTibow@users.noreply.github.com>
2021-02-15 15:22:29 -0800

    Update update.py

--- update.py
+++ update.py
@@ -1,24 +1,45 @@
 # Display updating image
 import displayio
 import board
+
 epd = board.DISPLAY
 with open("/updating.bmp", "rb") as bitmap_file:
-  bitmap = displayio.OnDiskBitmap(bitmap_file)
-  tile_grid = displayio.TileGrid(bitmap, pixel_shader=displayio.ColorConverter())
-  group = displayio.Group()
-  group.append(tile_grid)
-  epd.show(group)
-  epd.refresh()
+    bitmap = displayio.OnDiskBitmap(bitmap_file)
+    tile_grid = displayio.TileGrid(bitmap, pixel_shader=displayio.ColorConverter())
+    group = displayio.Group()
+    group.append(tile_grid)
+    epd.show(group)
+    epd.refresh()
 # Initial neopixel config
 import digitalio
+
 neopixels_enabled = digitalio.DigitalInOut(board.NEOPIXEL_POWER)
 neopixels_enabled.switch_to_output()
-neopixels_enabled.value = False # It is false, but it's also LOW.
+neopixels_enabled.value = False  # It is false, but it's also LOW.
 import neopixel
+
 builtin_neopixels = neopixel.NeoPixel(board.NEOPIXEL, 4)
 external_neopixels = neopixel.NeoPixel(board.A1, 30)
-builtin_neopixels.fill((0, 255, 255))
-external_neopixels.fill((128, 0, 255))
+# Pattern
+builtin_neopixel_left = 1
+builtin_neopixel_right = 2
+external_neopixel_left = 10
+external_neopixel_right = 11
+
+
+def render_wifi():
+    builtin_neopixels.fill((0, 255, 255))
+    builtin_neopixels[builtin_neopixel_left] = (255, 255, 255)
+    builtin_neopixels[builtin_neopixel_right] = (255, 255, 255)
+    external_neopixels.fill((128, 0, 255))
+    for i in range(8, 30):
+        if i > builtin_neopixel_left - 4 and i < builtin_neopixel_left:
+            external_neopixels[i] = (255, 255, 255)
+
+
 # Wait
 import time
-time.sleep(5)
+
+start_time = time.monotonic()
+while time.monotonic() - start_time < 5:
+    render_wifi()
