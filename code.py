76632a59b1f86425aa6f02f998777ce8b84ecce2
Kendell R <KTibow@users.noreply.github.com>
2021-02-19 14:40:24 -0800

    Update code.py

--- code.py
+++ code.py
@@ -207,9 +207,7 @@ while True:
         update_grocy()
         last_grocy_update = time.monotonic()
     if (
-        not magtag.peripherals.button_a_pressed
         and magtag.peripherals.button_b_pressed
-        and not magtag.peripherals.button_c_pressed
         and magtag.peripherals.button_d_pressed
     ):
         magtag.peripherals.neopixel_disable = False
