16145caa553ec49b4f79d1a18e96d5c0c9c59159
Kendell R <KTibow@users.noreply.github.com>
2021-02-23 11:50:52 -0800

    Actually update boot.py

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ try:
     os.stat("/latest_boot.py")
     with open("/boot.py", "w") as boot:
         with open("/latest_boot.py", "r") as new_boot:
-            pass
+            boot.write(new_boot.read())
+            boot.flush()
 except Exception:
     pass
