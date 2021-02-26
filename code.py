ab7b89da709ed3688e04634f45fc1798351fcab8
Kendell R <KTibow@users.noreply.github.com>
2021-02-26 10:09:24 -0800

    Add some boot stuff

--- code.py
+++ code.py
@@ -14,5 +14,21 @@ except Exception:
 from adafruit_magtag.magtag import MagTag # Main MagTag
 import alarm # Deep sleep
 
-mt = MagTag()
+mt = MagTag(rotation=180)
 wakeup_cause = alarm.wake_alarm
+if wakeup_cause is None:
+    print("Initial boot!")
+    mt.set_background("booting.bmp")
+    mt.add_text(
+        text_font="segoe-ui-12.pcf",
+        text_anchor_point=(0.5, 0),
+        text_position=(64, 148),
+    )
+    mt.set_text("< WiFi >")
+
+mt.network.connect()
+
+if wakeup_cause is None:
+    mt.set_text("< Data >")
+
+# mt.exit_and_deep_sleep(1)
